5faec5e384abee43ae39c5f8b282688584bc9a05
Lawrence Anthony <lawrence1992.pr@gmail.com>
2025-09-03 16:06:40 +0530

    added clerk for authentication

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,11 @@
-// import { Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "../components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import Header from "@/components/header";
+import { Toaster } from "sonner";
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Splito",
@@ -15,10 +18,19 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/logos/logo-s.png" sizes="any" />
       </head>
-      {/* <body className={`${inter.className}`}> */}
-      <body>
-        <Header />
-        <main className="min-h-screen">{children}</main>
+      <body className={`${inter.className}`}>
+        <ClerkProvider
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+        >
+          <ConvexClientProvider>
+            <Header />
+            <main className="min-h-screen">
+              <Toaster richColors />
+
+              {children}
+            </main>
+          </ConvexClientProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
