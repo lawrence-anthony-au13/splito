e54377de3264db9fc3135006571dcd5e927ec68c
lawrence.anthony <lawrence.anthony@cheersin.com>
2025-09-03 15:31:59 +0530

    adding convex API and data hander

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "../components/header";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Splito",
@@ -10,11 +11,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="hydrated">
       <head>
         <link rel="icon" href="/logos/logo-s.png" sizes="any" />
       </head>
-      <body className={`${inter.className}`}>{children}</body>
+      {/* <body className={`${inter.className}`}> */}
+      <body>
+        <Header />
+        <main className="min-h-screen">{children}</main>
+      </body>
     </html>
   );
 }
